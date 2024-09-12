3642a2a748b4bbe81241b67558d3d4d1c5e30281
FelipeLuz-codes <felipeluzfreitas@gmail.com>
2024-09-12 22:40:10 +0000

    strings test

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,13 +1,13 @@
 resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela a variável "resumo"
-
+print (resumo);
 
 # Imprima na tela apenas a segunda letra da variável
-
+resumo[1];
 
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
+resumo[25:26];
 
 # Imprima na tela o trecho final da variável
 
